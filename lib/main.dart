b4c97fa8ef7621901a26d8e2ba77461dd53ad3fc
mahima <mahima.stackup@gmail.com>
2024-10-31 17:40:50 +0530

    asd

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,104 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color.fromARGB(255, 243, 242, 245)),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: ''),
+      home: WelcomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        child: const Icon(Icons.add),
-      ),
-      body: const Center(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'welcome to Halodek',
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
+          children: [
+            Spacer(),
+            // Circle Avatar (Profile Image)
+            CircleAvatar(
+              radius: 60,
+              backgroundColor: Colors.orange[100],
+              child: CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('assets/images/avatar.png'),
               ),
             ),
+            SizedBox(height: 20),
+
+            // Welcome Text
             Text(
-              'Halodek support sending and receiving a variety of media:text,photos,videos,documents,and location, as well as vooice calls.',
+              'Welcome to Halodek',
               style: TextStyle(
-                  fontSize: 15,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(height: 10),
+
+            // Description Text
+            Text(
+              'Halodek supports sending and receiving a variety of media: text, photos, videos, documents, and location, as well as voice calls.',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.black54,
+              ),
+            ),
+            Spacer(),
+
+            // Get Started Button
+            ElevatedButton(
+              onPressed: () {
+                // Add navigation logic here
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.orange,
+                padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              child: Text(
+                "Let's Get Started",
+                style: TextStyle(
+                  fontSize: 16,
                   fontWeight: FontWeight.bold,
-                  color: Colors.grey),
-            )
+                  color: Colors.white,
+                ),
+              ),
+            ),
+
+            SizedBox(height: 20),
+
+            // Login Text Button
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Already have an account? "),
+                GestureDetector(
+                  onTap: () {
+                    // Add login navigation logic here
+                  },
+                  child: Text(
+                    "Login",
+                    style: TextStyle(
+                      color: Colors.orange,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Spacer(),
           ],
         ),
       ),
